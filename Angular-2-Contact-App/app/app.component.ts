2945976713b500c90874b94a8b3a0a8685534892
manojsripadi <manojsripadi@gmail.com>
2017-09-03 20:33:25 +0530

    Update app.component.ts

--- Angular-2-Contact-App/app/app.component.ts
+++ Angular-2-Contact-App/app/app.component.ts
@@ -15,6 +15,9 @@ import { Component } from '@angular/core';
         .alignright{
           color: purple
         }
+       #cart{
+          float:right
+        }
        `]
 })
 export class AppComponent {
